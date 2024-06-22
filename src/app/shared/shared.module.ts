d93c78bdf6f9bdfc138561eb974b6117041df8f5
juan jose suarez <suarezorregojuanjose@gmail.com>
2024-06-21 20:44:28 -0500

    Home

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuLateralComponent } from './components/molecules/menu-lateral/menu-lateral.component';
+import { FlechaDespliegueComponent } from './components/atoms/flecha-despliegue/flecha-despliegue.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MenuLateralComponent,
+    FlechaDespliegueComponent
+  ],
   imports: [
+  
     CommonModule
+  ],
+  exports: [
+    MenuLateralComponent,
+    FlechaDespliegueComponent
   ]
 })
 export class SharedModule { }
