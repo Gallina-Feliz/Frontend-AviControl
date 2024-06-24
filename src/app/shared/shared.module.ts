676da3d377ef1154bfa4aa5b4c20c2bbb537ce5b
Juan Jose <suarezorregojuanjose@gmail.com>
2024-06-23 22:13:12 -0500

    Funcionalidades

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { MenuLateralComponent } from './components/molecules/menu-lateral/menu-lateral.component';
 import { FlechaDespliegueComponent } from './components/atoms/flecha-despliegue/flecha-despliegue.component';
 import { ButtonDismissComponent } from './components/atoms/button-dismiss/button-dismiss.component';
+import { TablaSeleccionarComponent } from './components/molecules/tabla-seleccionar/tabla-seleccionar.component';
+import { TablaGallinasComponent } from './components/molecules/tabla-gallinas/tabla-gallinas.component';
+import { AlertComponent } from './components/organisms/alert/alert.component';
 
 
 
@@ -11,7 +14,10 @@ import { ButtonDismissComponent } from './components/atoms/button-dismiss/button
   declarations: [
     MenuLateralComponent,
     FlechaDespliegueComponent,
-    ButtonDismissComponent
+    ButtonDismissComponent,
+    TablaSeleccionarComponent,
+    TablaGallinasComponent,
+    AlertComponent
   ],
   imports: [
   
@@ -20,7 +26,10 @@ import { ButtonDismissComponent } from './components/atoms/button-dismiss/button
   exports: [
     MenuLateralComponent,
     FlechaDespliegueComponent,
-    ButtonDismissComponent
+    ButtonDismissComponent,
+    TablaSeleccionarComponent,
+    TablaGallinasComponent,
+    AlertComponent
   ]
 })
 export class SharedModule { }
