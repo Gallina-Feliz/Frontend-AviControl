f07565b18e0a8bb01181b79515a0d7313176c3c3
Sebastian Agudelo <agudelocortess@gmail.com>
2024-07-13 13:58:22 -0500

    Header y side bar

--- src/app/shared/components/molecules/side-nav/side-nav.component.ts
+++ src/app/shared/components/molecules/side-nav/side-nav.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.css'
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit{
+  list=[
+{
+  number: '1',
+  name: 'home',
+  icon: 'fa-solid fa-house',
 
+},
+{
+  number: '2',
+  name: 'Analytics',
+  icon: 'fa-solid fa-chart-line',
+
+},
+{
+  number: '3',
+  name: 'Products',
+  icon: 'fa-solid fa-box',
+
+},
+{
+  number: '4',
+  name: 'Order',
+  icon: 'fa-solid fa-shopping',
+
+},
+{
+  number: '5',
+  name: 'Settings',
+  icon: 'fa-solid fa-gear',
+
+},
+{
+  number: '6',
+  name: 'About',
+  icon: 'fa-solid fa-circle-info',
+
+},
+{
+  number: '7',
+  name: 'Contact',
+  icon: 'fa-solid fa-phone',
+
+},
+  ];
+
+constructor(){}
+
+ngOnInit(): void {
+    
+}
 }
