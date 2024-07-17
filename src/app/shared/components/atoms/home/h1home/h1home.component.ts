ac8cdb879aae5a71173fef3bb2ccdb425771975d
mariafernanda <mariafernandaa7516@gmail.com>
2024-07-16 19:45:10 -0500

    home-admin

---
+++ src/app/shared/components/atoms/home/h1home/h1home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-h1home',
+  templateUrl: './h1home.component.html',
+  styleUrl: './h1home.component.css'
+})
+export class h1homeComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,16 @@ import { FlechaDespliegueComponent } from './components/atoms/flecha-despliegue/
 import { UrlComponent } from './components/atoms/404/url/url.component';
 import { H1Component } from './components/atoms/404/h1/h1.component';
 import { DecriptionComponent } from './components/atoms/404/decription/decription.component';
+import { H2Component } from './components/atoms/home/h2/h2.component';
+import { PComponent } from './components/atoms/home/p/p.component';
+import { ButtonComponent } from './components/atoms/home/button/button.component';
+import { HomeComponent } from './components/molecules/home/home.component';
+import { h1homeComponent } from './components/atoms/home/h1home/h1home.component';
+import { H1adminComponent } from './components/atoms/homeadmin/h1admin/h1admin.component';
+
+import { LogoadminComponent } from './components/atoms/homeadmin/logoadmin/logoadmin.component';
+import { PadminComponent } from './components/atoms/homeadmin/padmin/padmin.component';
+import { AdminComponent } from './components/molecules/admin/admin.component';
 
 
 
@@ -15,7 +25,16 @@ import { DecriptionComponent } from './components/atoms/404/decription/decriptio
     FlechaDespliegueComponent,
     UrlComponent,
     H1Component,
-    DecriptionComponent
+    DecriptionComponent,
+    PComponent,
+    ButtonComponent,
+    HomeComponent,
+    h1homeComponent,
+    H1adminComponent,
+    LogoadminComponent,
+    PadminComponent,
+    H2Component,
+    AdminComponent 
   ],
   imports: [
   
@@ -23,7 +42,15 @@ import { DecriptionComponent } from './components/atoms/404/decription/decriptio
   ],
   exports: [
     MenuLateralComponent,
-    FlechaDespliegueComponent
-  ]
-})
+    FlechaDespliegueComponent,
+    UrlComponent,
+    H1Component,
+    DecriptionComponent,
+    H2Component,
+    PComponent,
+    ButtonComponent,
+    HomeComponent,
+    h1homeComponent,
+    AdminComponent 
+]})
 export class SharedModule { }
