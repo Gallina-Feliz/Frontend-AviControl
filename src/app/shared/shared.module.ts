543f23e9b775eb1f1e00d9d42268bd5f60e6b360
Juan Jose <suarezorregojuanjose@gmail.com>
2024-07-23 20:31:01 -0500

    elimine camponentes innesesarios

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { FlechaDespliegueComponent } from './components/atoms/flecha-despliegue/flecha-despliegue.component';
 import { ButtonDismissComponent } from './components/atoms/button-dismiss/button-dismiss.component';
-import { TablaSeleccionarComponent } from './components/molecules/tabla-seleccionar/tabla-seleccionar.component';
-import { TablaGallinasComponent } from './components/molecules/tabla-gallinas/tabla-gallinas.component';
 import { AlertComponent } from './components/organisms/alert/alert.component';
-import { DescripcionGalponesComponent } from './components/molecules/descripcion-galpones/descripcion-galpones.component';
-import { DescripcionGallinasComponent } from './components/molecules/descripcion-gallinas/descripcion-gallinas.component';
+
 import { HeaderComponent } from './components/molecules/header/header.component';
 import { SideNavComponent } from './components/molecules/side-nav/side-nav.component';
 
@@ -16,13 +13,7 @@ import { SideNavComponent } from './components/molecules/side-nav/side-nav.compo
 
 @NgModule({
   declarations: [
-    FlechaDespliegueComponent,
-    ButtonDismissComponent,
-    TablaSeleccionarComponent,
-    TablaGallinasComponent,
     AlertComponent,
-    DescripcionGalponesComponent,
-    DescripcionGallinasComponent,
     HeaderComponent,
     SideNavComponent
   ],
@@ -31,13 +22,7 @@ import { SideNavComponent } from './components/molecules/side-nav/side-nav.compo
     CommonModule
   ],
   exports: [
-    FlechaDespliegueComponent,
-    ButtonDismissComponent,
-    TablaSeleccionarComponent,
-    TablaGallinasComponent,
     AlertComponent,
-    DescripcionGalponesComponent,
-    DescripcionGallinasComponent,
     HeaderComponent,
     SideNavComponent
   ]
