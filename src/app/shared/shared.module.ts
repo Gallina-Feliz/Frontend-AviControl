e2f7f61eab67f86acdee3b77dd6e8997a2b0caf3
Juan Suarez <141379672+JuanJoSuarez08@users.noreply.github.com>
2024-07-24 20:59:55 -0500

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MenuLateralComponent } from './components/molecules/menu-lateral/menu-lateral.component';
-import { FlechaDespliegueComponent } from './components/atoms/flecha-despliegue/flecha-despliegue.component';
 import { UrlComponent } from './components/atoms/404/url/url.component';
 import { H1Component } from './components/atoms/404/h1/h1.component';
 import { DecriptionComponent } from './components/atoms/404/decription/decription.component';
