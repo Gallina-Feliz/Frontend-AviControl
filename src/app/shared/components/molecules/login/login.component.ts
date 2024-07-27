b1a1b3732f06fb43124c1f0f84e018eed82801f0
Tomas <Tomassena260522@gmail.com>
2024-07-27 10:51:07 -0500

    aun nada

--- src/app/shared/components/molecules/login/login.component.ts
+++ src/app/shared/components/molecules/login/login.component.ts
@@ -13,19 +13,30 @@ export class LoginComponent implements OnInit {
   constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
+    console.log('container:', this.container);
+    console.log('overlayBtn:', this.overlayBtn);
+
     if (this.overlayBtn && this.container) {
       this.renderer.listen(this.overlayBtn.nativeElement, 'click', () => {
+        console.log('Button clicked');
         this.toggleOverlay();
       });
     }
   }
 
   toggleOverlay(): void {
-    this.renderer.addClass(this.container.nativeElement, 'right-panel-active');
+    console.log('toggleOverlay called');
+
+    if (this.container.nativeElement.classList.contains('right-panel-active')) {
+      this.renderer.removeClass(this.container.nativeElement, 'right-panel-active');
+    } else {
+      this.renderer.addClass(this.container.nativeElement, 'right-panel-active');
+    }
+
     this.renderer.removeClass(this.overlayBtn.nativeElement, 'btnScaled');
     window.requestAnimationFrame(() => {
       this.renderer.addClass(this.overlayBtn.nativeElement, 'btnScaled');
     });
   }
-}
 
+}
