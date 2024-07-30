75fee03fcfbb1950b970d55dfddd22cba892c4b9
Juan Suarez <suarezorregojuanjose@gmail.com>
2024-07-30 18:59:42 -0500

    Limpio rama de develog

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MenuLateralComponent } from './components/molecules/menu-lateral/menu-lateral.component';
-import { FlechaDespliegueComponent } from './components/atoms/flecha-despliegue/flecha-despliegue.component';
+
 
 
 
 
 @NgModule({
   declarations: [
-    MenuLateralComponent,
-    FlechaDespliegueComponent
+    
   ],
   imports: [
   
     CommonModule
   ],
   exports: [
-    MenuLateralComponent,
-    FlechaDespliegueComponent
+    
   ]
 })
 export class SharedModule { }
