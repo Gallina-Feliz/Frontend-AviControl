cbdcc11670c584d6d42a11805ae053bee2f361be
mariafernanda <mariafernandaa7516@gmail.com>
2024-08-01 19:37:26 -0500

    url

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UrlComponent } from './components/atoms/url/url.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    UrlComponent
+  ],
+  imports: [
+  
+    CommonModule
+  ],
+  exports: [
+    UrlComponent
+  ]
+})
+export class SharedModule { }
