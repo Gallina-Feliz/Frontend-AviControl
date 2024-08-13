44a48abe3bacaced458a8001a6d10b8b7291d52c
tomas valencia <tomassena260522@gmail.com>
2024-08-13 18:57:30 -0500

    Inpues

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/molecules/login/login.component';
+import { ButtonsComponent } from './components/atoms/buttons/buttons.component';
+import { AvatarComponent } from './components/atoms/avatar/avatar.component';
+import { IconComponent } from './components/atoms/icon/icon.component';
+import { InputsComponent } from './components/atoms/inputs/inputs.component';
 
 
 
@@ -11,14 +15,21 @@ import { LoginComponent } from './components/molecules/login/login.component';
     
   
   
-    LoginComponent
+    LoginComponent,
+                  ButtonsComponent,
+                  AvatarComponent,
+                  IconComponent,
+                  InputsComponent
   ],
   imports: [
   
     CommonModule
   ],
   exports: [
-    LoginComponent
+    LoginComponent,
+    ButtonsComponent,
+    AvatarComponent,
+    IconComponent
   ]
 })
 export class SharedModule { }
