04926460cd9ffa50a2061143c9a493f56e87998b
Juan Jose Suarez <suarezorregojuanjose@gmail.com>
2024-08-22 00:03:50 -0500

    pagina de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../app/shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './features/salud-gallinas/pages/home/login-page/login-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component : LoginPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/features/salud-gallinas/salud-gallinas.module.ts
+++ src/app/features/salud-gallinas/salud-gallinas.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SaludGallinasRoutingModule } from './salud-gallinas-routing.module';
-import { MonitoreoComponent } from './monitoreo/monitoreo.component';
+
 
 
 @NgModule({
   declarations: [
-    MonitoreoComponent
+  
   ],
   imports: [
     CommonModule,
