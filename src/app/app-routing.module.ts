d88f350770dbfd88c3fd18212961e1f29dd16cae
Juan Jose Suarez <suarezorregojuanjose@gmail.com>
2024-08-22 19:08:40 -0500

    Paginas de home y login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../app/shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './features/salud-gallinas/pages/home/login-page/login-page.component';
-
+import { HomePageComponent} from "./features/salud-gallinas/pages/home/home-page/home-page.component";
 
 const routes: Routes = [
   {
     path: 'login',
     component : LoginPageComponent
   },
+  {
+    path: 'home',
+    component: HomePageComponent
+  }
 ];
 
 @NgModule({
