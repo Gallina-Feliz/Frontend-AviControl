c9817f3fd18f9e850e385f662a784e4b281930e1
samantha <sortegon96@gmail.com>
2024-08-24 09:18:49 -0900

    diagramas barras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/SharedModule';
+import { GraficaRadioComponent } from './shared/components/molecules/grafica-radio/grafica-radio.component';
+import { GraficaBarrasComponent } from './shared/components/molecules/grafica-barras/grafica-barras.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    GraficaRadioComponent,
+    GraficaBarrasComponent,
 
    
   ],
