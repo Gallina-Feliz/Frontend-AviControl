7546386e6cd59ac69b5d1c8a0be654d30869be3d
Juan Suarez <suarezorregojuanjose@gmail.com>
2024-09-09 16:18:34 -0500

    Formulario de la Alimetacion

---
+++ src/app/shared/components/templates/alimetacion-page-t/alimetacion-page-t.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alimetacion-page-t',
+  templateUrl: './alimetacion-page-t.component.html',
+  styleUrl: './alimetacion-page-t.component.css'
+})
+export class AlimetacionPageTComponent {
+
+}
